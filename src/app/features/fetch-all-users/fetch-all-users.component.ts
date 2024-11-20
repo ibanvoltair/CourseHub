4e4727d198a44ccd732e71ae6cda1376297fc95e
ibanvoltair <ibanvoltair@gmail.com>
2024-11-21 06:03:19 +0800

    add new component user-card

--- src/app/features/fetch-all-users/fetch-all-users.component.ts
+++ src/app/features/fetch-all-users/fetch-all-users.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { UserCardComponent } from '../../shared/user-card/user-card.component';
 
 @Component({
   selector: 'app-fetch-all-users',
   standalone: true,
-  imports: [],
+  imports: [ UserCardComponent ],
   templateUrl: './fetch-all-users.component.html',
-  styleUrl: './fetch-all-users.component.css'
+  styleUrl: './fetch-all-users.component.css',
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ] // Add this line
 })
 export class FetchAllUsersComponent {
 
