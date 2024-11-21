803294d0bc774d8a399e85693852f87c53be030d
ibanvoltair <ibanvoltair@gmail.com>
2024-11-22 04:32:26 +0800

    working fetch all user

--- src/app/features/fetch-all-users/fetch-all-users.component.ts
+++ src/app/features/fetch-all-users/fetch-all-users.component.ts
@@ -1,10 +1,11 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 import { UserCardComponent } from '../../shared/user-card/user-card.component';
 
 @Component({
   selector: 'app-fetch-all-users',
   standalone: true,
-  imports: [ UserCardComponent ],
+  imports: [ RouterModule, UserCardComponent ],
   templateUrl: './fetch-all-users.component.html',
   styleUrl: './fetch-all-users.component.css',
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ] // Add this line
--- src/app/shared/user-card/user-card.component.ts
+++ src/app/shared/user-card/user-card.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-user-card',
+  selector: 'user-card',
   standalone: true,
   imports: [],
   templateUrl: './user-card.component.html',
