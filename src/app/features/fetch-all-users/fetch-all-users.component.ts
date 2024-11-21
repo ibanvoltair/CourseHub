5160b976978e9df7228b65a175c264e0da713bd6
ibanvoltair <ibanvoltair@gmail.com>
2024-11-22 05:03:47 +0800

    resuable card user component

--- src/app/features/fetch-all-users/fetch-all-users.component.ts
+++ src/app/features/fetch-all-users/fetch-all-users.component.ts
@@ -1,11 +1,12 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { UserCardComponent } from '../../shared/user-card/user-card.component';
+import { TopNavComponent } from '../../shared/top-nav/top-nav.component';
 
 @Component({
   selector: 'app-fetch-all-users',
   standalone: true,
-  imports: [ RouterModule, UserCardComponent ],
+  imports: [ RouterModule, UserCardComponent, TopNavComponent ],
   templateUrl: './fetch-all-users.component.html',
   styleUrl: './fetch-all-users.component.css',
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ] // Add this line
